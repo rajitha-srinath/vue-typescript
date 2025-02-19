5053d953b5301a8fc154de08c4d7abea72c5e49b
rajithas <s.rajitha@directfn.com>
2025-02-20 00:54:12 +0530

    Initial files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    hello, {{ jobs[0].location }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Job from './types/job';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  setup() {
+    const jobs = ref<Job[]>([
+      {title: "raaji", id: '1', location: 'colombo', salary: 3000}
+    ]);
+
+    return {jobs}
+  }
+});
+</script>
+
+<style>
+
+</style>
