ec20bc04dcc02ae6e3127c61881fe0f7395c7567
rajitha-srinath <rajithasrinath94@gmail.com>
2025-02-21 08:56:14 +0530

    computed data

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -2,7 +2,7 @@
     <div class="job-list">
         <p>Ordered by {{ order }}</p>
         <ul>
-            <li v-for="job in jobs" :key="job.id">
+            <li v-for="job in orderedData" :key="job.id">
                 <h2>{{ job.title }} in {{ job.location }}</h2>
                 <div class="salary">
                     <p>{{ job.salary }}</p>
@@ -19,7 +19,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 import Job from '@/types/job';
 import OrderTerm from '@/types/OrderTerm';
 
@@ -33,7 +33,17 @@ export default defineComponent({
             required: true,
             type: String as PropType<OrderTerm>
         }
+    },
+    setup(props) {
+        const orderedData = computed(() => {
+            return [...props.jobs].sort((a: Job, b: Job) => {
+                return a[props.order] > b[props.order] ? 1 : -1
+            });
+        })
+
+        return { orderedData }
     }
+
 })
 </script>
 
@@ -41,6 +51,7 @@ export default defineComponent({
 ul {
     list-style-type: none;
 }
+
 .job-list li {
     margin: 20px 20px;
     background-color: #fff;
